b10850925c6a722b6be47f41309e98a0b9c4bc64
B10101 <b37710101@gmail.com>
2022-08-24 15:38:45 +0300

    first commit

---
+++ twitter.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import snscrape.modules.twitter as twit
+from time import sleep
+
+
+usernames = ['Quincy Larson' , 'freeCodeCamp.org', 'Jeff Delaney', 'Moshfegh Hamedani', 'Fireship']
+
+
+def get_tweets(username):
+    tweet_data = list()
+
+    number = 20
+
+    for i, tweets in enumerate(twit.TwitterSearchScraper('{}'.format(username)).get_items()):
+        if i>number:
+            break
+        tweet_data.append([tweets.date, tweets.content, tweets.user.username, tweets.url])
+
+    df = pd.DataFrame(tweet_data, columns = ['Date', 'Tweets', 'Username', 'Url'])
+    df.to_csv(f'{username}.csv', index=False,encoding='utf-8')
+   
+
+
+for i in usernames:
+    get_tweets(i)
